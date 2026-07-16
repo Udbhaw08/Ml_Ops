bd5f1998fb072f75095b505a78b76d38bdd71e4a
udbhaw08 <udbhawinsta@gmail.com>
2026-07-17 03:36:50 +0530

    Add pipeline evaluation and DVC lock configuration

---
+++ 11-End_to_End_project/Machine_Learning_pipeline/src/evaluate.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+import yaml
+import os
+import mlflow
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Load parameters from params.yaml
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def evaluate(data_path,model_path):
+    data=pd.read_csv(data_path)
+    X = data.drop(columns=["Outcome"])
+    y = data["Outcome"]
+
+    mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+
+    ## load the model from the disk
+    model=pickle.load(open(model_path,'rb'))
+
+    predictions=model.predict(X)
+    accuracy=accuracy_score(y,predictions)
+    ## log metrics to MLFLOW
+
+    with mlflow.start_run():
+        mlflow.log_metric("accuracy",accuracy)
+    print(f"Model accuracy:{accuracy}")
+
+if __name__=="__main__":
+    evaluate(params["data"],params["model"])
